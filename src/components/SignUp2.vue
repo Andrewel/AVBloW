1b0bc97fc5ca315e7396716407da170120b39b21
Andrewel <vlason15@gmail.com>
2019-01-30 16:00:46 +0300

    reg

--- src/components/SignUp2.vue
+++ src/components/SignUp2.vue
@@ -1,35 +1,58 @@
 <template>
   <div class="sign-up">
-    <img src="../assets/logo.png" width="300px" />
+    <img src="../assets/google-logo.png" width="100px" />
     <h3>Create a new account</h3>
-    <input
-      v-model="login"
-      type="text"
-      class="input"
-      placeholder="Login"
-      required
-    />
-    <br />
-    <input
-      v-model="email"
-      type="text"
-      class="input"
-      placeholder="Email"
-      required
-    />
-    <br />
-    <input
-      v-model="password"
-      type="password"
-      class="input"
-      placeholder="Password"
-      required
-    />
-    <br />
-    <v-btn v-on:click="signUp" class="button">Sign Up!</v-btn>
-    <v-btn class="button">
-      <router-link to="/login"> Back </router-link>
-    </v-btn>
+    <v-app>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-text-field
+          v-model="name"
+          :counter="10"
+          :rules="nameRules"
+          label="Name"
+          required
+        ></v-text-field>
+
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
+          required
+        ></v-text-field>
+
+        <v-select
+          v-model="select"
+          :items="items"
+          :rules="[v => !!v || 'Item is required']"
+          label="Item"
+          required
+        ></v-select>
+
+        <v-checkbox
+          v-model="checkbox"
+          :rules="[v => !!v || 'You must agree to continue!']"
+          label="Do you agree?"
+          required
+        ></v-checkbox>
+        <v-btn v-on:click="signUp" class="button">Sign Up!</v-btn>
+        <v-btn
+          v-on:click="signUp"
+          :disabled="!valid"
+          color="success"
+          @click="validate"
+        >
+          Validate
+        </v-btn>
+
+        <v-btn color="error" @click="reset"> Reset Form </v-btn>
+
+        <v-btn color="warning" @click="resetValidation">
+          Reset Validation
+        </v-btn>
+        <v-btn class="button">
+          <router-link to="/login"> Back </router-link>
+        </v-btn>
+      </v-form>
+    </v-app>
   </div>
 </template>
 
@@ -41,7 +64,21 @@ export default {
     return {
       login: "",
       email: "",
-      password: ""
+      password: "",
+      valid: true,
+      name: "",
+      nameRules: [
+        v => !!v || "Name is required",
+        v => (v && v.length <= 10) || "Name must be less than 10 characters"
+      ],
+      // email: "",
+      emailRules: [
+        v => !!v || "E-mail is required",
+        v => /.+@.+/.test(v) || "E-mail must be valid"
+      ],
+      select: null,
+      items: ["Belarus", "Russia", "Norway", "Ukrain"],
+      checkbox: false
     };
   },
   methods: {
@@ -55,6 +92,17 @@ export default {
         .catch(err => {
           alert(err.message);
         });
+    },
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true;
+      }
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
     }
   }
 };
@@ -63,6 +111,9 @@ export default {
 <style scoped>
 .sign-up {
   margin-top: 40px;
+}
+form {
+  align-self: center;
 }
 input {
   align-self: center;
